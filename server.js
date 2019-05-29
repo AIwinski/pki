a547234af1a8d61500b0c1a60dde78df3d7fdb9c
AIwinski <artur.iwinski@awww.software>
2019-05-29 17:13:47 +0200

    port

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.get("*", function(req, res) {
 let port = process.env.port || 3000;
 
 app.listen(port, () => {
-    console.log("Example app listening on port 3000!");
+    console.log("Example app listening on port: " + port);
 });
